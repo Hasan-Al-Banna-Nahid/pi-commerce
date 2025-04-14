e1e53572d73c39f878c1c10a0f15d99200050497
Hasan-Al-Banna-Nahid <iamnahid591998@gmail.com>
2025-04-14 13:16:15 +0600

    Done

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,24 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: process.env.NEXT_PUBLIC_FRONTEND_URL || "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
